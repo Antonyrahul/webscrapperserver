b04b951a49a8f7fde4748837f201df1d44e15c83
unknown <antonyrahul96@gmail.com>
2024-06-03 21:28:58 +0530

    browser for ubuntu

--- index.js
+++ index.js
@@ -20,7 +20,11 @@ app.use(cors({
   app.post('/search',async function(req,res){
     try{
    console.log(req.body)
-   const browser = await puppeteer.launch();
+   const browser = await puppeteer.launch(
+    {
+      executablePath: '/usr/bin/chromium-browser'
+    }
+   );
   const page = await browser.newPage();
   // await page.setViewport({width: 1860, height: 1400});
   // await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4427.0 Safari/537.36')
